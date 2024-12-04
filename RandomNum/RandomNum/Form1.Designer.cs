26bd590bf7a7069e5bde02f45fcbc88c4fa8a69f
HackerShann <hackershann.eth@gmail.com>
2024-12-03 22:09:25 -0500

    RandomNumber files added

---
+++ RandomNum/RandomNum/Form1.Designer.cs
@@ -0,0 +1,96 @@
+﻿namespace RandomNum
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Label1 = new System.Windows.Forms.Label();
+            this.txtNumberOfRandoms = new System.Windows.Forms.TextBox();
+            this.btnGenerate = new System.Windows.Forms.Button();
+            this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+            this.lblMessage = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // Label1
+            // 
+            this.Label1.AutoSize = true;
+            this.Label1.Location = new System.Drawing.Point(40, 82);
+            this.Label1.Name = "Label1";
+            this.Label1.Size = new System.Drawing.Size(374, 25);
+            this.Label1.TabIndex = 0;
+            this.Label1.Text = "Enter the number of random numbers:";
+            // 
+            // txtNumberOfRandoms
+            // 
+            this.txtNumberOfRandoms.Location = new System.Drawing.Point(646, 99);
+            this.txtNumberOfRandoms.Name = "txtNumberOfRandoms";
+            this.txtNumberOfRandoms.Size = new System.Drawing.Size(100, 31);
+            this.txtNumberOfRandoms.TabIndex = 1;
+            // 
+            // btnGenerate
+            // 
+            this.btnGenerate.Location = new System.Drawing.Point(414, 466);
+            this.btnGenerate.Name = "btnGenerate";
+            this.btnGenerate.Size = new System.Drawing.Size(167, 81);
+            this.btnGenerate.TabIndex = 2;
+            this.btnGenerate.Text = "Generate";
+            this.btnGenerate.UseVisualStyleBackColor = true;
+            this.btnGenerate.Click += new System.EventHandler(this.btnGenerate_Click);
+            // 
+            // lblMessage
+            // 
+            this.lblMessage.AutoSize = true;
+            this.lblMessage.Location = new System.Drawing.Point(40, 205);
+            this.lblMessage.Name = "lblMessage";
+            this.lblMessage.Size = new System.Drawing.Size(0, 25);
+            this.lblMessage.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1101, 628);
+            this.Controls.Add(this.lblMessage);
+            this.Controls.Add(this.btnGenerate);
+            this.Controls.Add(this.txtNumberOfRandoms);
+            this.Controls.Add(this.Label1);
+            this.Name = "Form1";
+            this.Text = "Random Number File Generator";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label Label1;
+        private System.Windows.Forms.TextBox txtNumberOfRandoms;
+        private System.Windows.Forms.Button btnGenerate;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog1;
+        private System.Windows.Forms.Label lblMessage;
+    }
+}
---
+++ RandomNum/RandomNum/Form1.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.IO;
+using System.Windows.Forms;
+
+namespace RandomNum
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnGenerate_Click(object sender, EventArgs e)
+        {
+            // Validate the number entered
+            if (!int.TryParse(txtNumberOfRandoms.Text, out int numberOfRandoms) || numberOfRandoms <= 0)
+            {
+                MessageBox.Show("Please enter a valid positive number.");
+                return;
+            }
+
+            // Show the SaveFileDialog to select file location and name
+            saveFileDialog1.Filter = "Text Files (*.txt)|*.txt|All Files (*.*)|*.*";
+            saveFileDialog1.Title = "Save Random Numbers File";
+            if (saveFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                string filePath = saveFileDialog1.FileName;
+                try
+                {
+                    // Write random numbers to the file
+                    Random rand = new Random();
+                    using (StreamWriter writer = new StreamWriter(filePath))
+                    {
+                        for (int i = 0; i < numberOfRandoms; i++)
+                        {
+                            int randomNumber = rand.Next(1, 101); // Generates numbers from 1 to 100
+                            writer.WriteLine(randomNumber);
+                        }
+                    }
+                    lblMessage.Text = "File saved successfully!";
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show($"Error: {ex.Message}");
+                }
+            }
+        }
+    }
+}
